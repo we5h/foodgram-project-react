6cb8007618e69144f10ce328900b4a32001c9fa4
Дмитрий Грибков <weshnicolas@gmail.com>
2023-06-01 10:31:53 +0300

    Users,Follow added to admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Follow
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ('email', 'first_name')
+    list_filter = ('email', 'first_name')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    empty_value_display = '-пусто-'
--- backend/users/models.py
+++ backend/users/models.py
@@ -40,6 +40,8 @@ class Follow(models.Model):
     )
 
     class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
         constraints = [
             models.UniqueConstraint(
                 fields=['user', 'author'],
